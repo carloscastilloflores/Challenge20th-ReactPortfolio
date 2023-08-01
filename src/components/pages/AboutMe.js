336496197a5a43cac7f58e6e5113088c2c63d34b
carloscastilloflores <125534814+carloscastilloflores@users.noreply.github.com>
2023-08-01 12:12:29 -0600

    Added About Me and Portfolio v1

---
+++ src/components/pages/AboutMe.js
@@ -0,0 +1,22 @@
+import React from 'react'; 
+
+export default function AboutMe() {
+    return (
+        <div> 
+            <h1> About Me </h1>
+            <p>
+            Hi there! My name is Carlos Castillo Flores, and I'm a designer and photographer based in Guadalajara, 
+            Mexico. I studied industrial design and have been applying my knowledge to create systems, products, 
+            and visuals for the last 5 years. As a professional photographer, I've had the opportunity to work with 
+            different brands to help them translate their vision to reality through stunning visuals that are both 
+            creative and effective.
+
+            Lately, I've been diving into the world of web development and applying my design abilities to create
+            beautiful and functional websites. I'm excited to continue learning and growing in this field, and I'm
+            always looking for new projects to work on. If you're interested in collaborating on a project or have
+            an idea you'd like to discuss, please feel free to connect with me! I'm always open to new opportunities
+            and would love to hear from you.
+            </p>
+        </div>
+    );
+}
---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,40 @@
+import React from 'react'; 
+
+export default function Portfolio() {
+    return (
+        <div class="item-container">
+        <h2>Work</h2>
+            <a href="https://rodrigorol.github.io/TuneSurf/index.html" class="box-item photofbr" >
+                <div>
+                    <h3>TuneSurf Application</h3>
+                    <span>Content experience</span>
+                </div>
+            </a>
+        
+            <a href="#" class="box-item radial-website">
+                <div>
+                    <h3>Radial Website</h3>
+                    <span>HTML & CSS</span>
+                </div>
+            </a>
+            <a href="#" class="box-item corona-challenge">
+                <div>
+                    <h3>Corona Challenge</h3>
+                    <span>Design Development</span>
+                </div>
+            </a>
+            <a href="#" class="box-item photo-editing">
+                <div>
+                    <h3>Editorial photography</h3>
+                    <span>Post-production editing</span>
+                </div>
+            </a>
+            <a href="#" class="box-item  creative-project">
+                <div>
+                    <h3>Album Art Miguel Calderón</h3>
+                    <span>Storytelling and visual work</span>
+                </div>
+            </a>
+        </div>
+    );
+}

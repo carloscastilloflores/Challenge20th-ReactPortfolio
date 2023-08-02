a688e3431ab13b3ae00c8dd53fab65723769b916
carloscastilloflores <125534814+carloscastilloflores@users.noreply.github.com>
2023-08-01 22:49:31 -0600

    added profile photo

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,22 +1,28 @@
 import React from 'react'; 
+import pfp from '../../images/Avatar.JPG'
 
 export default function AboutMe() {
     return (
-        <div className='container'> 
-            <h1> About Me </h1>
-            <p>
-            Hi there! My name is Carlos Castillo Flores, and I'm a designer and photographer based in Guadalajara, 
-            Mexico. I studied industrial design and have been applying my knowledge to create systems, products, 
-            and visuals for the last 5 years. As a professional photographer, I've had the opportunity to work with 
-            different brands to help them translate their vision to reality through stunning visuals that are both 
-            creative and effective.
+        <div className='container d-flex'> 
+            <div className=""> 
+                <img src={pfp} className="d-flex p-4" style={{width: '260px' }}/>
+            </div>
+            <div className="">
+                <h1> About Me </h1>
+                <p>
+                Hi there! My name is Carlos Castillo Flores, and I'm a designer and photographer based in Guadalajara, 
+                Mexico. I studied industrial design and have been applying my knowledge to create systems, products, 
+                and visuals for the last 5 years. As a professional photographer, I've had the opportunity to work with 
+                different brands to help them translate their vision to reality through stunning visuals that are both 
+                creative and effective.
 
-            Lately, I've been diving into the world of web development and applying my design abilities to create
-            beautiful and functional websites. I'm excited to continue learning and growing in this field, and I'm
-            always looking for new projects to work on. If you're interested in collaborating on a project or have
-            an idea you'd like to discuss, please feel free to connect with me! I'm always open to new opportunities
-            and would love to hear from you.
-            </p>
+                Lately, I've been diving into the world of web development and applying my design abilities to create
+                beautiful and functional websites. I'm excited to continue learning and growing in this field, and I'm
+                always looking for new projects to work on. If you're interested in collaborating on a project or have
+                an idea you'd like to discuss, please feel free to connect with me! I'm always open to new opportunities
+                and would love to hear from you.
+                </p>
+            </div>
         </div>
     );
 }
